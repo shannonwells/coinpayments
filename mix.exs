e89b9da2d83fd18d12ace974b2faedd095abcff8
shannonewells <shannon@carbonfive.com>
2018-03-21 08:24:57 -0700

    update mix.exs for publishing on Hex, add ex_docs dependency

--- mix.exs
+++ mix.exs
@@ -4,28 +4,38 @@ defmodule Coinpayments.Mixfile do
   def project do
     [
       app: :coinpayments,
-      version: "0.1.0",
+      version: "0.5.2",
       elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      description: description(),
+      source_url: "https://github.com/shannonwells/coinpayments_ex",
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
-
-  # Run "mix help deps" to learn about dependencies.
-  # {:dep_from_hexpm, "~> 0.3.0"},
-  # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
 
   defp deps do
     [
       {:tesla, "~> 0.10.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:mock, "~> 0.3.0", only: :test}
+    ]
+  end
+
+  defp description do
+    "A wrapper for the Coinpayments API"
+  end
+
+  defp package do
+    [
+      maintainers: ["Shannon Wells"],
+      links: %{"GitHub": "https://github.com/shannonwells/coinpayments_ex"},
+      licenses: ["MIT"]
     ]
   end
 end
